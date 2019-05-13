07cc743eaa99728481b96cb897a3d51636907328
Matthew Cameron <mattc@outlook.co.nz>
2019-05-13 13:51:47 +1200

    Update example scene to enable resetting of the ball

---
+++ Assets/Scripts/VR_SDK_Fix.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using UnityEngine;
+
+/// <summary>
+/// The VR simulation seems to stop running on my machine.
+/// This will re-activate it after a certain amount of time to save having to manually do it
+/// </summary>
+public class VR_SDK_Fix : MonoBehaviour
+{
+
+    [SerializeField] GameObject[] expectedSDKs;
+    [SerializeField] GameObject fallback;
+    [SerializeField] float timeToWait = 2.0f;
+
+    void Start()
+    {
+        StartCoroutine(checkSDKs());
+    }
+
+    IEnumerator checkSDKs()
+    {
+        yield return new WaitForSecondsRealtime(timeToWait);
+
+        bool anyActive = false;
+        foreach (var sdk in expectedSDKs)
+        {
+            anyActive |= sdk.activeInHierarchy;
+        }
+
+        if (!anyActive)
+        {
+            fallback.SetActive(true);
+        }
+    }
+}
