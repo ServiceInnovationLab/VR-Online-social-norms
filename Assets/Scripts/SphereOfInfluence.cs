dc58fcab7cdacdf90bb2d684c1ffefdab8b80e55
Matthew Cameron <mattc@outlook.co.nz>
2019-05-24 16:30:38 +1200

    Very small perspective change test

---
+++ Assets/Scripts/SphereOfInfluence.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using VRTK;
+
+public class SphereOfInfluence : MonoBehaviour
+{
+    public Transform target;
+
+
+    public void DoMove()
+    {
+        var pos = target.position;
+
+        Destroy(target.gameObject);
+        Destroy(gameObject);
+
+        var teleport = FindObjectOfType<VRTK_BasicTeleport>();        
+        teleport.ForceTeleport(pos);
+    }
+    
+}
