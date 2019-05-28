c4277f3cfefd8cbc9a5bc5318b6768bc4e4fed91
Matthew Cameron <mattc@outlook.co.nz>
2019-05-28 13:22:55 +1200

    Add ability to exit application with Escape key

---
+++ Assets/Scripts/ExitOnEscape.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class ExitOnEscape : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#else
+            Application.Quit();
+#endif
+        }
+    }
+}
