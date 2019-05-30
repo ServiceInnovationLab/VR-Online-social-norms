d9c3002bee55733746ccf93ff244c1e1d617a514
Matthew Cameron <mattc@outlook.co.nz>
2019-05-31 10:57:53 +1200

    Add potentially easier way to scroll tablet

---
+++ Assets/Scripts/ScrollWithTouchpad.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using VRTK;
+
+[RequireComponent(typeof(ScrollRect))]
+public class ScrollWithTouchpad : MonoBehaviour
+{
+    [SerializeField] VRTK_ControllerEvents controllerEvents;
+
+    ScrollRect scrollView;
+
+    private void Awake()
+    {
+        scrollView = GetComponent<ScrollRect>();
+
+        if (!controllerEvents)
+        {
+            Debug.LogError("No controller events are attached!");
+        }
+    }
+
+    void FixedUpdate()
+    {
+        var eventData = new PointerEventData(EventSystem.current) { scrollDelta = controllerEvents.GetTouchpadAxis() };
+
+        ExecuteEvents.ExecuteHierarchy(gameObject, eventData, ExecuteEvents.scrollHandler);
+    }
+}
