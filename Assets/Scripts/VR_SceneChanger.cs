d9161f78394d70781c4737c05d32dd7bfabadb39
Matthew Cameron <mattc@outlook.co.nz>
2019-05-31 12:31:21 +1200

    Add switching rooms

--- Assets/Scripts/VR_SceneChanger.cs
+++ Assets/Scripts/VR_SceneChanger.cs
@@ -11,15 +11,22 @@ public class VR_SceneChanger : MonoBehaviour
     [Tooltip("The colour to fade to when making the scene switch.")]
     [SerializeField] Color blinkToColor = Color.black;
 
+    [Tooltip("The delay before doing the teleport")]
+    [SerializeField] float delay = 0;
+
+    [SerializeField] string teleportToName;
+
     bool switched;
 
     public void SwitchScenes()
     {
-        if (!switched && scene != null && !string.IsNullOrEmpty(scene.ScenePath))
+        if (delay == 0f)
         {
-            DisableTeleporters();
-            StartCoroutine(DoSwitchScene());
-            switched = true;
+            DoSwitchScene();
+        }
+        else
+        {
+            Invoke(nameof(DoSwitchScene), delay);
         }
     }
 
@@ -31,7 +38,35 @@ public class VR_SceneChanger : MonoBehaviour
         }
     }
 
-    IEnumerator DoSwitchScene()
+    void DoSwitchScene()
+    {
+        if (!switched)
+        {
+            if (!string.IsNullOrWhiteSpace(teleportToName))
+            {
+                foreach (var location in FindObjectsOfType<VR_SceneTeleportTo>())
+                {
+                    if (location.gameObject.name == teleportToName)
+                    {
+                        // Teleport here
+                        var teleporter = FindObjectOfType<VRTK_BasicTeleport>();
+                        teleporter.Teleport(location.transform, location.transform.position);
+
+                        return;
+                    }
+                }
+            }
+
+            if (!string.IsNullOrEmpty(scene.ScenePath))
+            {
+                DisableTeleporters();
+                StartCoroutine(DoSwitchSceneLoad());
+                switched = true;
+            }
+        }
+    }
+
+    IEnumerator DoSwitchSceneLoad()
     {
         VRTK_SDK_Bridge.HeadsetFade(blinkToColor, 0, true);
 
