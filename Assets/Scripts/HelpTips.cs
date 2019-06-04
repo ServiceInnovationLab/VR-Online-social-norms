af8d165f79ce8c5fff906e2e96c9f8e618c29e59
Matthew Cameron <mattc@outlook.co.nz>
2019-06-04 13:04:27 +1200

    Add easy way to show and change controller tooltips

---
+++ Assets/Scripts/HelpTips.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "Help/ControllerHelpTips")]
+public class HelpTips : ScriptableObject
+{
+    public string helpName;
+
+    public ControllerHelp leftController;
+    public ControllerHelp rightController;
+}
---
+++ Assets/Scripts/ShowControlsOnGrab.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using VRTK;
+
+[RequireComponent(typeof(VRTK_InteractableObject))]
+public class ShowControlsOnGrab : MonoBehaviour
+{
+    [SerializeField] string controlHelpName;
+
+    VRTK_InteractableObject interactableObject;
+    HelpTipsManager manager;
+    string previousHelp;
+
+    void Awake()
+    {
+        interactableObject = GetComponent<VRTK_InteractableObject>();
+        manager = FindObjectOfType<HelpTipsManager>();
+    }
+
+    void OnEnable()
+    {
+        interactableObject.InteractableObjectGrabbed += InteractableObject_Grabbed;
+        interactableObject.InteractableObjectUngrabbed += InteractableObject_Ungrabbed;
+    }
+
+    void OnDisable()
+    {
+        interactableObject.InteractableObjectGrabbed -= InteractableObject_Grabbed;
+        interactableObject.InteractableObjectUngrabbed -= InteractableObject_Ungrabbed;
+    }
+
+    void InteractableObject_Ungrabbed(object sender, InteractableObjectEventArgs e)
+    {
+        manager.ShowHelp(previousHelp);
+    }
+
+    void InteractableObject_Grabbed(object sender, InteractableObjectEventArgs e)
+    {
+        previousHelp = manager.GetCurrentHelp();
+        manager.ShowHelp(controlHelpName);
+        manager.ShowHelp(true);
+    }
+}
