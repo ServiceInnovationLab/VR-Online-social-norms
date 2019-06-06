c2ba5b2952254e4a0da75876ee6850e641824709
Matthew Cameron <mattc@outlook.co.nz>
2019-06-06 12:00:03 +1200

    Update receviver room to set NPC position and setup the tablet and interactions

--- Assets/Scripts/PerspectiveChanger.cs
+++ Assets/Scripts/PerspectiveChanger.cs
@@ -28,9 +28,13 @@ public class PerspectiveChanger : MonoBehaviour
 
     [SerializeField] UnityEvent afterTeleport;
 
+    [SerializeField] bool scalePosition;
+
     public void DoTeleport()
     {
-        var teleportPosition = target.position + offset;
+        bool scaleRoom = sceneObjects && newSceneScale > 0;
+
+        var teleportPosition = (target.position * (scaleRoom && scalePosition ? newSceneScale : 1)) + offset;
 
         var teleporter = FindObjectOfType<VRTK_BasicTeleport>();
         var originalBlinkDelay = teleporter.distanceBlinkDelay;
@@ -39,12 +43,9 @@ public class PerspectiveChanger : MonoBehaviour
         teleporter.distanceBlinkDelay = blinkDistance;
         teleporter.blinkTransitionSpeed = blinkTransition;
 
-        if (sceneObjects)
+        if (scaleRoom)
         {
-            if (newSceneScale > 0)
-            {
-                sceneObjects.localScale = new Vector3(newSceneScale, newSceneScale, newSceneScale);
-            }
+            sceneObjects.localScale = new Vector3(newSceneScale, newSceneScale, newSceneScale);
         }
 
         teleporter.Teleport(target, teleportPosition);
