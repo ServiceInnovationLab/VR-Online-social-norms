df7bda173572d055607c291a66b7d88d50130a6c
Matthew Cameron <mattc@outlook.co.nz>
2019-06-10 13:38:29 +1200

    Allow controller tooltips to swap around

--- Assets/Scripts/ShowControlsOnGrab.cs
+++ Assets/Scripts/ShowControlsOnGrab.cs
@@ -1,10 +1,33 @@
 ﻿using UnityEngine;
 using VRTK;
 
+/// <summary>
+/// Desribes what direction the controls are specified for
+/// </summary>
+public enum ControlDirection
+{
+    /// <summary>
+    /// Control order doesn't matter
+    /// </summary>
+    Either,
+
+    /// <summary>
+    /// Controls are setup for this object in the left hand
+    /// </summary>
+    Left,
+
+    /// <summary>
+    /// Controls are setup for this object in the right hand
+    /// </summary>
+    Right
+}
+
 [RequireComponent(typeof(VRTK_InteractableObject))]
 public class ShowControlsOnGrab : MonoBehaviour
 {
     [SerializeField] string controlHelpName;
+    [SerializeField] ControlDirection controlsDirection = ControlDirection.Either;
+
 
     VRTK_InteractableObject interactableObject;
     HelpTipsManager manager;
@@ -46,7 +69,21 @@ public class ShowControlsOnGrab : MonoBehaviour
     {
         GetHelpManager();
         previousHelp = manager.GetCurrentHelp();
-        manager.ShowHelp(controlHelpName);
+
+        bool swapLeftAndRight = false;
+
+        if (controlsDirection != ControlDirection.Either)
+        {
+            bool pickedUpWithLeft = e.interactingObject.gameObject.name.Contains("LeftController");
+
+            if (pickedUpWithLeft && controlsDirection == ControlDirection.Right ||
+                !pickedUpWithLeft && controlsDirection == ControlDirection.Left)
+            {
+                swapLeftAndRight = true;
+            }
+        }
+
+        manager.ShowHelp(controlHelpName, swapLeftAndRight);
         manager.ShowHelp(true);
     }
 }
