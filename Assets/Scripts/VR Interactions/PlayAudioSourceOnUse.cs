b1eefe6bb36a5186a040a070ceb6003b72a16b77
Matthew Cameron <mattc@outlook.co.nz>
2019-06-13 10:53:18 +1200

    Add clicking sound when using keyboard/mouse

---
+++ Assets/Scripts/VR Interactions/PlayAudioSourceOnUse.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using VRTK;
+
+[RequireComponent(typeof(VRTK_InteractableObject), typeof(AudioSource))]
+public class PlayAudioSourceOnUse : MonoBehaviour
+{
+    VRTK_InteractableObject interactableObject;
+    AudioSource audioSource;
+
+    private void Awake()
+    {
+        interactableObject = GetComponent<VRTK_InteractableObject>();
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void OnEnable()
+    {
+        interactableObject.InteractableObjectUsed += InteractableObjectUsed;
+    }
+
+    private void OnDisable()
+    {
+        interactableObject.InteractableObjectUsed -= InteractableObjectUsed;
+    }
+
+    private void InteractableObjectUsed(object sender, InteractableObjectEventArgs e)
+    {
+        audioSource.Play();
+    }
+
+}
