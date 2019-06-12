7d52fee0f0a5f04070e26bc0f1124a6231ca6163
Matthew Cameron <mattc@outlook.co.nz>
2019-06-12 16:34:05 +1200

    Put scripts into folders
    Tidy up the scripts a bit.. could do with better folders in the future

---
+++ Assets/Scripts/VectorExtensions.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public static class VectorExtensions
+{
+
+    /// <summary>
+    /// Returns the vector with just the X and Z components
+    /// </summary>
+    public static Vector3 XZ(this Vector3 vector)
+    {
+        return new Vector3(vector.x, 0, vector.z);
+    }
+
+    /// <summary>
+    /// Returns a vector with just the absolute value of each component
+    /// </summary>
+    public static Vector3 Abs(this Vector3 vector)
+    {
+        return new Vector3(Mathf.Abs(vector.x), Mathf.Abs(vector.y), Mathf.Abs(vector.z));
+    }
+}
