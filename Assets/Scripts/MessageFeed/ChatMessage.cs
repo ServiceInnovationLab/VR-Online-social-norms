899bf91d78f96fd5ca2929cb389da4b01a5fd145
Matthew Cameron <mattc@outlook.co.nz>
2019-06-19 17:04:11 +1200

    Population of second monitor with videos and in-game chat

---
+++ Assets/Scripts/MessageFeed/ChatMessage.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ChatMessage : MonoBehaviour
+{
+    public string from;
+    public string message;
+
+    [SerializeField] Text messageText;
+    [SerializeField] Text fromText;
+    [SerializeField] float timeToLive;
+
+    float time = 0;
+
+    public Text GetMessageTextField()
+    {
+        return messageText;
+    }
+
+    private void Awake()
+    {
+        fromText.text = "[All] " + from;
+        messageText.text = message;
+    }
+
+    private void FixedUpdate()
+    {
+        time += Time.deltaTime;
+
+        if (time > timeToLive)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
