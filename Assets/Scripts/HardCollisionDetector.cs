8a35df6e792e0d4cf9f3e6c49b7a590af27b72cb
Matthew Cameron <mattc@outlook.co.nz>
2019-06-24 11:38:50 +1200

    Quick little impact test

---
+++ Assets/Scripts/HardCollisionDetector.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class HardCollisionDetector : MonoBehaviour
+{
+    public UnityEvent onHardCollision;
+
+    [SerializeField] float minimumVelocity = 9;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.relativeVelocity.magnitude > minimumVelocity)
+        {
+            onHardCollision?.Invoke();
+        }
+    }
+}
