91c1eb52c71f50965b4be5ca00216f93d309cae1
Matthew Cameron <mattc@outlook.co.nz>
2019-06-26 16:11:09 +1200

    Maybe finally fix desk being able to be teleported on

---
+++ Assets/Scripts/Editor/FindObjectsWithTagWindow.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class FindObjectsWithTagWindow : EditorWindow
+{
+    string tag = "IncludeTeleport";
+
+   [MenuItem("Window/Find Objects with Tag")]
+    static void Awake()
+    {
+        GetWindow<FindObjectsWithTagWindow>("Find Objects with Tag").Show();
+    }
+
+    void OnGUI()
+    {
+        tag = GUILayout.TextField(tag);
+
+        if (GUILayout.Button("Find"))
+        {
+            GameObject[] objects = GameObject.FindGameObjectsWithTag(tag);
+            Selection.objects = objects;
+
+            foreach (var obj in objects)
+            {
+                Debug.Log(obj.name, obj);
+            }
+        }
+    }
+
+   
+}
