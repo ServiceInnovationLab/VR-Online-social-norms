5af6da0e5571ecda803d71c52d11932fe8c794bb
Matthew Cameron <mattc@outlook.co.nz>
2019-08-22 11:29:09 +1200

    Allow getting the trackpad and trigger objects for changing the colour

---
+++ Assets/Scripts/DetechScrollToBottom.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(ScrollRect))]
+public class DetechScrollToBottom : MonoBehaviour
+{
+    [SerializeField] UnityEvent onReachedThreshold = new UnityEvent();
+    [SerializeField] float threshold = 0.8f;
+
+    ScrollRect rect;
+
+    private void Awake()
+    {
+        rect = GetComponent<ScrollRect>();
+    }
+
+    private void OnEnable()
+    {
+        rect.onValueChanged.AddListener(OnScroll);
+    }
+
+    private void OnDisable()
+    {
+        rect.onValueChanged.RemoveListener(OnScroll);
+    }
+
+    private void OnScroll(Vector2 amount)
+    {
+        Debug.Log(rect.verticalNormalizedPosition);
+        if (rect.verticalNormalizedPosition >= threshold)
+        {
+            onReachedThreshold?.Invoke();
+        }
+    }
+}
---
+++ Assets/Scripts/SteamControllerColour.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using Valve.VR;
+using VRTK;
+using System.Collections;
+
+public class SteamControllerColour : MonoBehaviour
+{
+    [SerializeField] VRTK_SDKManager sdkManager;
+    [SerializeField] SteamVR_RenderModel controller1;
+    [SerializeField] SteamVR_RenderModel controller2;
+
+    MeshRenderer touchpad1Renderer;
+    MeshRenderer trigger1Renderer;
+
+    MeshRenderer touchpad2Renderer;
+    MeshRenderer trigger2Renderer;
+
+    bool steamLoaded = false;
+    bool gettingControllers = false;
+
+    private void Awake()
+    {
+        sdkManager.LoadedSetupChanged += SdkManager_LoadedSetupChanged;
+    }
+
+    private void SdkManager_LoadedSetupChanged(VRTK_SDKManager sender, VRTK_SDKManager.LoadedSetupChangeEventArgs e)
+    {
+        if (e.currentSetup == null)
+            return;
+
+        steamLoaded = e.currentSetup.name.Contains("Steam");
+    }
+
+    private void FixedUpdate()
+    {
+        if (!steamLoaded || gettingControllers)
+            return;
+
+        if (!hasModels())
+        {
+            StartCoroutine(GetControllers());
+        }
+    }
+
+    private bool hasModels()
+    {
+        return touchpad1Renderer && trigger1Renderer
+                    && touchpad2Renderer && trigger2Renderer;
+    }
+
+    IEnumerator GetControllers()
+    {
+        gettingControllers = true;
+        while (true)
+        {
+            yield return new WaitForSeconds(0.25f);
+
+            touchpad1Renderer = controller1.transform.Find("trackpad")?.GetComponent<MeshRenderer>();
+            trigger1Renderer = controller1.transform.Find("trigger")?.GetComponent<MeshRenderer>();
+
+            touchpad2Renderer = controller2.transform.Find("trackpad")?.GetComponent<MeshRenderer>();
+            trigger2Renderer = controller2.transform.Find("trigger")?.GetComponent<MeshRenderer>();
+
+            if (hasModels())
+            {
+                break;
+            }
+        }
+        gettingControllers = false;
+    }
+
+}
