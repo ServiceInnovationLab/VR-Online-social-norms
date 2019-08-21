ac058e2e152278dcde20121a3afa992a41aa0aec
Matthew Cameron <mattc@outlook.co.nz>
2019-08-21 14:52:27 +1200

    Update play area limited teleport to check for collisions!

--- Assets/Scripts/VR Interactions/PlayAreaLimitedTeleport.cs
+++ Assets/Scripts/VR Interactions/PlayAreaLimitedTeleport.cs
@@ -53,7 +53,7 @@ public class PlayAreaLimitedTeleport : VRTK_HeightAdjustTeleport
             }
         }
 
-     /*   if (inPlayArea && checkForCollisions)
+        if (inPlayArea && checkForCollisions)
         {
             if (!bodyCollider)
             {
@@ -63,18 +63,33 @@ public class PlayAreaLimitedTeleport : VRTK_HeightAdjustTeleport
             if (bodyCollider)
             {
                 var originalY = destinationPosition.y;
-                destinationPosition.y = GetTeleportY(target, destinationPosition) + bodyCollider.radius + 0.05f;
+                destinationPosition.y = GetTeleportY(target, destinationPosition) + bodyCollider.radius * 2 + 0.05f;
 
                 int count = Physics.OverlapCapsuleNonAlloc(destinationPosition, destinationPosition + Vector3.up * bodyCollider.height, bodyCollider.radius * 2, hitColliders, ~0, QueryTriggerInteraction.Ignore);
+
+              /*  var newItem = GameObject.CreatePrimitive(PrimitiveType.Sphere).transform;
+                newItem.position = destinationPosition;
+                newItem.localScale = new Vector3(bodyCollider.radius * 2, bodyCollider.radius * 2, bodyCollider.radius * 2);
+                Destroy(newItem.GetComponent<Collider>());
+
+                newItem = GameObject.CreatePrimitive(PrimitiveType.Sphere).transform;
+                newItem.position = destinationPosition + Vector3.up * bodyCollider.height;
+                newItem.localScale = new Vector3(bodyCollider.radius * 2, bodyCollider.radius * 2, bodyCollider.radius * 2);
+                Destroy(newItem.GetComponent<Collider>());*/
 
                 if (count > 0)
                 {
                     inPlayArea = false;
+
+                    /*for (int i = 0; i < count; i++)
+                    {
+                        Debug.LogError("Can't teleport due to ", hitColliders[i]);
+                    }*/
                 }
 
                 destinationPosition.y = originalY;
             }
-        }*/
+        }
 
         return inPlayArea && base.ValidLocation(target, destinationPosition);
     }
