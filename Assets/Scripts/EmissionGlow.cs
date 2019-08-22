812c2a6a6521b963de9825b68a7ff98a645bcbb5
Matthew Cameron <mattc@outlook.co.nz>
2019-08-22 13:28:30 +1200

    Allow the VIve controller buttons to show as a different colour and glow

---
+++ Assets/Scripts/EmissionGlow.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EmissionGlow : MonoBehaviour
+{
+    public float fadeTime = 1;
+    public float minGlow = 0.2f;
+    public float maxGlow = 1.0f;
+    public Color emissionColour;
+    public bool showEmission;
+    public bool animate;
+
+    new Renderer renderer;
+    float sign = 1;
+    
+    float alpha;
+
+    private void Awake()
+    {
+        renderer = GetComponent<Renderer>();
+        renderer.material.EnableKeyword("_EMISSION");
+
+        alpha = minGlow;
+        showEmission = false;
+        animate = false;
+    }
+
+    void Update()
+    {
+        if (showEmission && !animate)
+        {
+            alpha = maxGlow;
+            sign = -1;
+        }
+        else if (showEmission && animate)
+        {
+            float step = (maxGlow - minGlow) * Time.deltaTime * (1 / fadeTime);
+            step *= sign;
+
+            if (alpha + step > maxGlow)
+            {
+                alpha = maxGlow;
+                sign *= -1;
+            }
+            else if (alpha + step <= minGlow)
+            {
+                alpha = minGlow;
+                sign *= -1;
+            }
+            else
+            {
+                alpha += step;
+            }
+        }
+        else
+        {
+            alpha = 0;
+            sign = 1;
+        }
+
+       
+        renderer.material.SetColor("_EmissionColor", emissionColour * Mathf.LinearToGammaSpace(alpha));
+    }
+}
--- Assets/Scripts/SteamControllerColour.cs
+++ Assets/Scripts/SteamControllerColour.cs
@@ -6,17 +6,42 @@ using System.Collections;
 public class SteamControllerColour : MonoBehaviour
 {
     [SerializeField] VRTK_SDKManager sdkManager;
-    [SerializeField] SteamVR_RenderModel controller1;
-    [SerializeField] SteamVR_RenderModel controller2;
+    [SerializeField] SteamVR_RenderModel controllerLeft;
+    [SerializeField] SteamVR_RenderModel controllerRight;
 
-    MeshRenderer touchpad1Renderer;
-    MeshRenderer trigger1Renderer;
+    [SerializeField] VRTK_Pointer leftPointer;
+    [SerializeField] VRTK_Pointer rightPointer;
 
-    MeshRenderer touchpad2Renderer;
-    MeshRenderer trigger2Renderer;
+    [SerializeField] Color triggerButtonColour = Color.yellow;
+    [SerializeField] Color touchpadButtonColour = Color.blue;
+
+    EmissionGlow touchpad1Renderer;
+    EmissionGlow trigger1Renderer;
+
+    EmissionGlow touchpad2Renderer;
+    EmissionGlow trigger2Renderer;
 
     bool steamLoaded = false;
-    bool gettingControllers = false;
+    bool gettingControllers1 = false;
+    bool gettingControllers2 = false;
+    [SerializeField] bool animateTouchpad = false;
+    [SerializeField]  bool animateTrigger = false;
+    [SerializeField] bool showTriggers = true;
+
+    public void SetGlowTouchpad(bool glow)
+    {
+        animateTouchpad = glow;
+    }
+
+    public void SetGlowTrigger(bool glow)
+    {
+        animateTrigger = glow;
+    }
+
+    public void SetShowTrigger(bool show)
+    {
+        showTriggers = show;
+    }
 
     private void Awake()
     {
@@ -33,40 +58,85 @@ public class SteamControllerColour : MonoBehaviour
 
     private void FixedUpdate()
     {
-        if (!steamLoaded || gettingControllers)
+        if (!steamLoaded)
             return;
 
-        if (!hasModels())
+        if (hasModels1())
         {
-            StartCoroutine(GetControllers());
+            touchpad1Renderer.showEmission = leftPointer.enabled && leftPointer.enableTeleport;
+            touchpad1Renderer.animate = animateTouchpad;
+            trigger1Renderer.animate = animateTrigger;
+            trigger1Renderer.showEmission = showTriggers;
+        }
+        else if (!gettingControllers1)
+        {
+            StartCoroutine(GetController1());
+        }
+
+
+        if (hasModels2())
+        {
+            touchpad2Renderer.showEmission = rightPointer.enabled && rightPointer.enableTeleport;
+            touchpad2Renderer.animate = animateTouchpad;
+            trigger2Renderer.animate = animateTrigger;
+            trigger2Renderer.showEmission = showTriggers;
+        }
+        else if(!gettingControllers2)
+        {
+            StartCoroutine(GetController2());
         }
     }
 
-    private bool hasModels()
+    private bool hasModels1()
     {
-        return touchpad1Renderer && trigger1Renderer
-                    && touchpad2Renderer && trigger2Renderer;
+        return touchpad1Renderer && trigger1Renderer;
     }
 
-    IEnumerator GetControllers()
+    private bool hasModels2()
     {
-        gettingControllers = true;
+        return touchpad2Renderer && trigger2Renderer;
+    }
+
+    IEnumerator GetController1()
+    {
+        gettingControllers1 = true;
         while (true)
         {
             yield return new WaitForSeconds(0.25f);
 
-            touchpad1Renderer = controller1.transform.Find("trackpad")?.GetComponent<MeshRenderer>();
-            trigger1Renderer = controller1.transform.Find("trigger")?.GetComponent<MeshRenderer>();
+            touchpad1Renderer = controllerLeft.transform.Find("trackpad")?.gameObject?.GetOrCreateComponent<EmissionGlow>();
+            trigger1Renderer = controllerLeft.transform.Find("trigger")?.gameObject?.GetOrCreateComponent<EmissionGlow>();
 
-            touchpad2Renderer = controller2.transform.Find("trackpad")?.GetComponent<MeshRenderer>();
-            trigger2Renderer = controller2.transform.Find("trigger")?.GetComponent<MeshRenderer>();
-
-            if (hasModels())
-            {
+            if (hasModels1())
                 break;
-            }
         }
-        gettingControllers = false;
+        gettingControllers1 = false;
+
+        touchpad1Renderer.emissionColour = touchpadButtonColour;
+        touchpad1Renderer.maxGlow = touchpadButtonColour.a;
+        trigger1Renderer.emissionColour = triggerButtonColour;
+        trigger1Renderer.maxGlow = triggerButtonColour.a;
+    }
+
+    IEnumerator GetController2()
+    {
+        gettingControllers2 = true;
+        while (true)
+        {
+            yield return new WaitForSeconds(0.25f);
+
+            touchpad2Renderer = controllerRight.transform.Find("trackpad")?.gameObject?.GetOrCreateComponent<EmissionGlow>();
+            trigger2Renderer = controllerRight.transform.Find("trigger")?.gameObject?.GetOrCreateComponent<EmissionGlow>();
+
+            if (hasModels2())
+                break;
+        }
+        gettingControllers2 = false;
+
+        touchpad2Renderer.emissionColour = touchpadButtonColour;
+        touchpad2Renderer.maxGlow = touchpadButtonColour.a;
+        trigger2Renderer.emissionColour = triggerButtonColour;
+        trigger2Renderer.maxGlow = triggerButtonColour.a;
     }
 
 }
