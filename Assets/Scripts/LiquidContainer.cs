87dcd461f71920408d0f3b61bab3fecb07f9355b
Matthew Cameron <mattc@outlook.co.nz>
2019-08-30 15:15:32 +1200

    Add in the start of some scripts

---
+++ Assets/Scripts/LiquidContainer.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class LiquidContainer : MonoBehaviour
+{
+    [SerializeField] Transform liquidParent;
+    [SerializeField] Transform liquidMesh;
+
+    [SerializeField] float sloshSpeed = 60;
+    [SerializeField] float rotateSpeed = 15;
+
+    [SerializeField] float difference = 25;
+
+    private void Update()
+    {
+        Slosh();
+
+        liquidMesh.Rotate(Vector3.up * rotateSpeed * Time.deltaTime, Space.Self);
+    }
+
+    private void Slosh()
+    {
+        var inverseRotation = Quaternion.Inverse(transform.rotation);
+
+        var finalRotation = Quaternion.RotateTowards(liquidParent.localRotation, inverseRotation, sloshSpeed * Time.deltaTime).eulerAngles;
+
+        finalRotation.x = ClampRotationValue(finalRotation.x, difference);
+        finalRotation.z = ClampRotationValue(finalRotation.z, difference);
+
+        liquidParent.localEulerAngles = finalRotation;
+    }
+
+    private float ClampRotationValue(float value, float difference)
+    {
+        if (value > 180)
+        {
+            return Mathf.Clamp(value, 360 - difference, 360);
+        }
+
+        return Mathf.Clamp(value, 0, difference);
+    }
+    
+}
