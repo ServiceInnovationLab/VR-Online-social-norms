c98d0ddded3270aee4a742322df724279bb9ae43
Matthew Cameron <mattc@outlook.co.nz>
2019-09-11 13:20:23 +1200

    Add in audio mixer and adjustment script

---
+++ Assets/Scripts/Audio/VolumeAdjuster.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+
+public class VolumeAdjuster : MonoBehaviour
+{
+    [SerializeField] float startingVolume = 0;
+    [SerializeField] float raisedVolume = 0;
+    [SerializeField] AudioMixer audioMixer;
+
+    private void Awake()
+    {
+        audioMixer.SetFloat("MasterVolume", startingVolume);
+    }
+
+    public void RaiseVolume()
+    {
+        audioMixer.SetFloat("MasterVolume", raisedVolume);
+    }
+
+
+}
