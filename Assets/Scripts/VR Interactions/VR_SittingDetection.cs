fa71a0bb1184a202c7728e70a7920a8ed3c4bd45
Matthew Cameron <mattc@outlook.co.nz>
2019-09-12 10:29:37 +1200

    Update sitting detection

--- Assets/Scripts/VR Interactions/VR_SittingDetection.cs
+++ Assets/Scripts/VR Interactions/VR_SittingDetection.cs
@@ -1,21 +1,29 @@
 ﻿using UnityEngine;
+using UnityEngine.Events;
 using VRTK;
 using Valve.VR;
 
 public class VR_SittingDetection : MonoBehaviour
 {
-    [SerializeField] int bufferSize = 90;
+    int bufferSize = 90;
+    [SerializeField] bool disableOnSittingDetected = true;
+    [SerializeField] UnityEvent sittingDetected;
+    [SerializeField] TrackedObject objectNeededForSitting;
+    [SerializeField] int secondsToSettle = 1;
     Transform headsetTransform;
 
     SteamVR_RingBuffer<float> heights;
 
     int seen;
+    int settledSeconds;
+
+    float lastStandingHeight;
+
+    bool detectSitting = false;
 
     private void Awake()
     {
         VRTK_SDKManager.AttemptAddBehaviourToToggleOnLoadedSetupChange(this);
-
-        heights = new SteamVR_RingBuffer<float>(bufferSize);
     }
 
     private void OnDestroy()
@@ -23,13 +31,15 @@ public class VR_SittingDetection : MonoBehaviour
         VRTK_SDKManager.AttemptRemoveBehaviourToToggleOnLoadedSetupChange(this);
     }
 
-
     private void OnEnable()
     {
         headsetTransform = VRTK_DeviceFinder.HeadsetTransform();
-    }
 
-    void Update()
+        bufferSize = Mathf.CeilToInt(1 / Time.fixedUnscaledDeltaTime);
+        heights = new SteamVR_RingBuffer<float>(bufferSize);
+    }
+    
+    void FixedUpdate()
     {
         heights.Add(headsetTransform.position.y);
 
@@ -39,12 +49,67 @@ public class VR_SittingDetection : MonoBehaviour
         {
             seen = 0;
 
-            float sum = 0;
+            float currentHeight = 0;
+
+            float heading = 0;
+            float last = heights.GetAtIndex(0);
+
             for (int i = 0; i < bufferSize; i++)
             {
-                sum += heights.GetAtIndex(i);
+                var height = heights.GetAtIndex(i);
+
+                currentHeight += height;
+
+                if (detectSitting)
+                {
+                    heading += height - last;
+                    last = height;
+                }
             }
-            sum /= bufferSize;
+            currentHeight /= bufferSize;
+
+            if (!detectSitting)
+            {
+                lastStandingHeight = currentHeight;
+            }
+            else
+            {
+                if (currentHeight < lastStandingHeight && Mathf.Approximately(heading, 0))
+                {
+                    settledSeconds++;
+
+                    if (settledSeconds >= secondsToSettle)
+                    {
+                        OnSittingDetected();
+                    }
+                }
+                else
+                {
+                    settledSeconds = 0;
+                }
+            }
+        }
+    }
+
+    private void OnSittingDetected()
+    {
+        sittingDetected?.Invoke();
+
+        if (disableOnSittingDetected)
+        {
+            enabled = false;
+        }
+    }
+
+    public void DetectSitting()
+    {
+        detectSitting = true;
+        settledSeconds = 0;
+
+        if (objectNeededForSitting && !objectNeededForSitting.IsTracked)
+        {
+            OnSittingDetected();
+            return;
         }
     }
 }
