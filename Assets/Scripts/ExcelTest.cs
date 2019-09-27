13e02cfdabbe33cfdbc936e0f3b3540b3395baad
Matthew Cameron <mattc@outlook.co.nz>
2019-09-27 13:30:17 +1200

    Add in sample Excel code for reading data

---
+++ Assets/Scripts/ExcelTest.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.IO;
+using ExcelDataReader;
+
+public class ExcelTest : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        /*
+        using (var stream = File.Open("D:\\Sample Excel.xlsx", FileMode.Open, FileAccess.Read))
+        {
+            using (var reader = ExcelReaderFactory.CreateReader(stream))
+            {
+                var result = reader.AsDataSet();
+
+                var sms = result.Tables["SMS"];
+                var twitter = result.Tables["Twitter"];
+
+                foreach (System.Data.DataRow row in sms.Rows)
+                {
+                    int i = 0;
+                }
+            }
+        }*/
+    }
+}
