35d6d6c7c05ce73015f1fffd111d5fae2ff7d0a4
Matthew Cameron <mattc@outlook.co.nz>
2019-10-08 11:35:57 +1300

    Add in initial scrollable Twitter steam and emoji assets from Karyn

--- Assets/Scripts/SenderRoomInteraction.cs
+++ Assets/Scripts/SenderRoomInteraction.cs
@@ -1,20 +1,93 @@
 ﻿using UnityEngine;
 using UnityEngine.Events;
+using System.Collections;
 
 public class SenderRoomInteraction : MonoBehaviour
 {
     public UnityEvent OnEnterPov;
 
-    public UnityEvent[] interactions;
+    public UnityEvent OnCompleted;
+
+    public ScrollRectDetector[] requiredScrolls;
+    public FlashingImage[] images;
+
+    [SerializeField] float timeBeforeActivatingNext = 1.0f;
 
     public void OnPovSwitch()
     {
         OnEnterPov?.Invoke();
     }
 
-    public void TriggerInteraction(int index)
+    private void Awake()
     {
-        interactions[index]?.Invoke();
+        foreach (var image in images)
+        {
+            image.gameObject.SetActive(false);
+        }
+
+        foreach (var detector in requiredScrolls)
+        {
+            detector.enabled = false;
+        }
+    }
+
+    private void OnEnable()
+    {
+        StartCoroutine(DoChecks());
+
+        foreach (var detector in requiredScrolls)
+        {
+            detector.enabled = true;
+        }
+    }
+
+    private IEnumerator DoChecks()
+    {
+        bool activate = true;
+
+        bool[] previous = new bool[requiredScrolls.Length];
+        for (int i = 0; i < previous.Length; i++)
+        {
+            previous[i] = false;
+        }
+
+        while (activate)
+        {
+            yield return new WaitForFixedUpdate();
+
+            for (int i = 0; i < requiredScrolls.Length; i++)
+            {
+                bool done = requiredScrolls[i].ScrollingDone;
+
+                if (images[i])
+                {
+                    images[i].gameObject.SetActive(!done && activate);
+                }
+
+                if (done)
+                {
+                    if (!previous[i])
+                    {
+                        yield return new WaitForSeconds(timeBeforeActivatingNext);
+                    }
+                }
+                else
+                {
+                    activate = false;
+                }
+
+                previous[i] = done;
+            }
+
+            // All are complete if we still get here!
+            if (activate)
+            {
+                OnCompleted?.Invoke();
+                yield break;
+            }
+
+            activate = true;
+        }
     }
 
 }
