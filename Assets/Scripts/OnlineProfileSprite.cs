71687b8eb1eceaf1bc061946e6903b42d5e94ccc
Matthew Cameron <mattc@outlook.co.nz>
2019-10-29 09:37:34 +1300

    Have it use the loaded sender profile picture on twitter and the reply

---
+++ Assets/Scripts/OnlineProfileSprite.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class OnlineProfileSprite : MonoBehaviour
+{
+    [SerializeField] SocialMediaScenarioTextType profileType;
+
+    private void Start()
+    {
+        var profile = SocialMediaScenarioPicker.Instance.CurrentScenario.GetProfile(profileType);
+
+        GetComponent<Image>().sprite = profile.picture;
+    }
+}
