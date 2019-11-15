8b9aa85e34f2a2d5f247f547f9bffdf82db42809
Matthew Cameron <mattc@outlook.co.nz>
2019-11-15 14:38:16 +1300

    Revert "Update network grab to try and keep things more in sync"
    This reverts commit 8de7246110cec590f40d84c3b56e37bd7f7652ce.

--- Assets/Scripts/Multiplayer/NetworkedInteractGrab.cs
+++ Assets/Scripts/Multiplayer/NetworkedInteractGrab.cs
@@ -2,13 +2,6 @@
 using Mirror;
 using UnityEngine;
 
-
-/// <summary>
-/// Helps with objects that are picked up and thrown.
-/// 
-/// To make things easier, items that are picked up become owned by the client to make every other client match up with what's happening here. 
-/// As this is just trying things out this is fine.
-/// </summary>
 public class NetworkedInteractGrab : NetworkBehaviour
 {
 
@@ -37,20 +30,6 @@ public class NetworkedInteractGrab : NetworkBehaviour
         var id = GetComponent<NetworkIdentity>();
 
         VrPlayer.MyPlayer.CmdPickUp(id, VrPlayer.MyPlayer.netIdentity);
-    }
-
-    private void OnCollisionEnter(Collision collision)
-    {
-        // If someone throws this object and it touches something, make those objects follow what happened here
-        if (!hasAuthority)
-            return;
-
-        var id = collision.gameObject.GetComponent<NetworkIdentity>();
-
-        if (id)
-        {
-            VrPlayer.MyPlayer.CmdPickUp(id, VrPlayer.MyPlayer.netIdentity);
-        }
     }
 
 }
